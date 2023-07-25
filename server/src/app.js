c7ba80a42dc22b17d5c752314113fd93cb62bd80
Tornike <itorn9777@gmail.com>
2023-07-25 14:15:54 +0400

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// init express & path
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+export { app };
